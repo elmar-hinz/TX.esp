8282787ec4c0d0b91c11f620e0d8860f0901c129
Elmar Hinz <me@example.org>
2013-03-08 10:24:05 +0100

    after ter upload and setting up github

--- ext_tables.php
+++ ext_tables.php
@@ -1,6 +1,5 @@
 <?php if (!defined('TYPO3_MODE')) { die ('Access denied.'); }
 
-t3lib_extMgm::addStaticFile($_EXTKEY,'static/breadcrumb/', 'esp breadcrumb example');
-t3lib_extMgm::addStaticFile($_EXTKEY,'static/stored_procedure/', 'Stored Procedure');
+ t3lib_extMgm::addStaticFile($_EXTKEY,'static/breadcrumb/', 'esp breadcrumb example');
 
 ?>
